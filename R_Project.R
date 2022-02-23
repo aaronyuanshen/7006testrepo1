e9aeb09109463650bf3dac3a8e2665db54c19a51
Aaron Shen <99938637+aaronyuanshen@users.noreply.github.com>
2022-02-23 11:32:46 +1000

    first commit, no comments

---
+++ R_Project.R
@@ -0,0 +1,12 @@
+# Step 1: Data import
+
+library(readxl)
+data1 <- read_excel("OD_data.xlsx", range = "B28:CU317")
+data1 <- select(data1, -2)
+
+#Step 2: Data tidying
+
+data1 <- data1 %>%
+  mutate(Time = Time / 60) %>%
+  pivot_longer(-Time, names_to = "Well", values_to = "OD")
+
